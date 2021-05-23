964ec49f94c7df039c37ab6bfa54cef3bceda8dd
andrianazz <andrianwahyu41@gmail.com>
2021-05-23 09:26:15 +0700

    Belajar Fluttix

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = "a3de8231d409f1f108a05ac71bbdf8b0";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
+File imageFileToUpload;
